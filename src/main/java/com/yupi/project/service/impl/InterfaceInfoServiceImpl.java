7755fb236658828bd9ab887025d677e9f4c02d83
cr1314258 <2757244337@qq.com>
2024-03-23 21:43:49 +0800

    frontend and backend intial

---
+++ src/main/java/com/yupi/project/service/impl/InterfaceInfoServiceImpl.java
@@ -0,0 +1,65 @@
+package com.yupi.project.service.impl;
+import java.util.Date;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.yupi.project.common.ErrorCode;
+import com.yupi.project.exception.BusinessException;
+import com.yupi.project.mapper.InterfaceInfoMapper;
+import com.yupi.project.model.entity.InterfaceInfo;
+import com.yupi.project.service.InterfaceInfoService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+
+/**
+* @author Estar pro
+* @description 针对表【interface_info(接口信息)】的数据库操作Service实现
+* @createDate 2024-03-23 19:42:59
+*/
+@Service
+public class InterfaceInfoServiceImpl extends ServiceImpl<InterfaceInfoMapper, InterfaceInfo>
+    implements InterfaceInfoService {
+
+    @Override
+    public void validInterfaceInfo(InterfaceInfo interfaceInfo, boolean add) {
+     String name = interfaceInfo.getName();
+     String description = interfaceInfo.getDescription();
+     String url = interfaceInfo.getUrl();
+     String requestHeader = interfaceInfo.getRequestHeader();
+     String responseHeader = interfaceInfo.getResponseHeader();
+     String method = interfaceInfo.getMethod();
+
+        if (interfaceInfo == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 创建时，所有参数必须非空
+        if (add) {
+            if (StringUtils.isAnyBlank()) {
+                throw new BusinessException(ErrorCode.PARAMS_ERROR);
+            }
+        }
+        // 如果接口名称不为空且长度大于50,抛出参数错误的异常,错误信息为"名称过长"
+        if (StringUtils.isNotBlank(name) && name.length() > 50) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "内容过长");
+        }
+        if (StringUtils.isNotBlank(description)) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "请填写描述信息");
+        }
+        if (StringUtils.isNotBlank(url)) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "请填写接口地址");
+        }
+        if (StringUtils.isNotBlank(responseHeader)) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "请填写请求头");
+        }
+        if (StringUtils.isNotBlank(requestHeader)) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "请填写响应头");
+        }
+        if (StringUtils.isNotBlank(method)) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "请填写请求类型");
+        }
+    }
+
+}
+
+
+
+
