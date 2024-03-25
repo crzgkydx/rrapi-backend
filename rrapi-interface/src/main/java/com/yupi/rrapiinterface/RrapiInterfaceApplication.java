1e1991e97058c5ed6f59e25b3796be254c33549d
cr1314258 <2757244337@qq.com>
2024-03-25 12:04:17 +0800

    http call interface

---
+++ rrapi-interface/src/main/java/com/yupi/rrapiinterface/RrapiInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.yupi.rrapiinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RrapiInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RrapiInterfaceApplication.class, args);
+    }
+
+}
